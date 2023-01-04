e4b6924ab11b6509ecb842d793df4c68284d6fd9
prern4321 <prerna_dutt@yahoo.com>
2023-01-04 15:39:59 +0000

    uploading the final code

--- hopsitals.sql
+++ hopsitals.sql
@@ -1,18 +1,18 @@
 DROP TABLE hospitals;
 
 CREATE TABLE hospitals (
-OrganisationID INT NOT NULL,
-OrganisationCode VARCHAR(30) NOT NULL,
-Sector VARCHAR(30) NOT NULL,
-OrganisationName VARCHAR(30) NOT NULL,
-Address1 VARCHAR(30) NOT NULL,        
-Address2 VARCHAR(30) NOT NULL,         
-Address3 VARCHAR(30) NOT NULL,         
-City     VARCHAR(30) NOT NULL,        
-County   VARCHAR(30) NOT NULL,         
-Postcode VARCHAR(30) NOT NULL,         
-Latitude  FLOAT NOT NULL,       
-Longitude FLOAT NOT NULL
+organisationid INT NOT NULL,
+organisationcode VARCHAR(30) NOT NULL,
+sector VARCHAR(30) NOT NULL,
+organisationname VARCHAR(30) NOT NULL,
+address1 VARCHAR(30) NOT NULL,        
+address2 VARCHAR(30) NOT NULL,         
+address3 VARCHAR(30) NOT NULL,         
+city     VARCHAR(30) NOT NULL,        
+county   VARCHAR(30) NOT NULL,         
+postcode VARCHAR(30) NOT NULL,         
+latitude  FLOAT NOT NULL,       
+longitude FLOAT NOT NULL
 );
 
 SELECT * FROM hospitals ;
